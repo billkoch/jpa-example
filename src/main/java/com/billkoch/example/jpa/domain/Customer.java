881179de4ba5f5739ad4a9bacd6fd2d0b5bf808e
Bill Koch <koch.149@osu.edu>
2011-07-09 11:48:24 -0400

    Updated the CustomerRepository JPA implementation to return the Customer's ID after it has been persisted.

--- src/main/java/com/billkoch/example/jpa/domain/Customer.java
+++ src/main/java/com/billkoch/example/jpa/domain/Customer.java
@@ -1,5 +1,22 @@
 package com.billkoch.example.jpa.domain;
 
-public class Customer {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
--- src/main/java/com/billkoch/example/jpa/repository/impl/CustomerRepositoryJpaImpl.java
+++ src/main/java/com/billkoch/example/jpa/repository/impl/CustomerRepositoryJpaImpl.java
@@ -1,17 +1,22 @@
 package com.billkoch.example.jpa.repository.impl;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
 
 import com.billkoch.example.jpa.domain.Customer;
 import com.billkoch.example.jpa.repository.CustomerRepository;
 
+@Repository
 public class CustomerRepositoryJpaImpl implements CustomerRepository {
 
+	@PersistenceContext
 	protected EntityManager entityManager;
 
 	@Override
 	public String save(Customer customer) {
 		entityManager.persist(customer);
-		return "";
+		return customer.getId();
 	}
 }
