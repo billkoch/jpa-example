a053131ad95ee0a6ad5f95a0d12e12c242ae7d98
Bill Koch <koch.149@osu.edu>
2011-07-27 18:26:41 -0400

    Created a unit test to ensure the JPA DAO interacts with the JPA Entity Manager when persisting new Account objects

--- src/main/java/com/billkoch/example/jpa/repository/impl/AccountRepositoryJpaImpl.java
+++ src/main/java/com/billkoch/example/jpa/repository/impl/AccountRepositoryJpaImpl.java
@@ -1,11 +1,16 @@
 package com.billkoch.example.jpa.repository.impl;
 
+import javax.persistence.EntityManager;
+
 import com.billkoch.example.jpa.domain.Account;
 
 public class AccountRepositoryJpaImpl implements AccountRepository {
 
+	protected EntityManager entityManager;
+	
 	@Override
 	public String createNewAccount(Account account) {
+		entityManager.persist(account);
 		return null;
 	}
 }
--- src/test/java/com/billkoch/example/jpa/repository/impl/AccountRepositoryJpaImplTest.java
+++ src/test/java/com/billkoch/example/jpa/repository/impl/AccountRepositoryJpaImplTest.java
@@ -1,14 +1,38 @@
 package com.billkoch.example.jpa.repository.impl;
 
+import static org.mockito.Mockito.verify;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.billkoch.example.jpa.domain.Account;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AccountRepositoryJpaImplTest {
 
+	private AccountRepository uut;
+	
+	@Mock
+	private EntityManager mockEntityManager;
+	
+	private Account account;
+	
+	@Before
+	public void setup() {
+		uut = new AccountRepositoryJpaImpl();
+		((AccountRepositoryJpaImpl) uut).entityManager = mockEntityManager;
+		account = new Account();
+	}
+	
 	@Test
 	public void createNewAccountShouldInteractWithTheEntityManager() {
-		AccountRepository uut = new AccountRepositoryJpaImpl();
-		String accountId = uut.createNewAccount(new Account());
+		uut.createNewAccount(account);
+
+		verify(mockEntityManager).persist(account);
 	}
 }
