3c26edc47c6dc29c2b24320d3b918aa88f2cd3f5
Bill Koch <koch.149@osu.edu>
2011-07-27 22:59:29 -0400

    Revert "Moved the AccountRepository interface out of the impl package...not sure why I put it there in the first place..."
    This reverts commit aacf37ba101406b634b8fd3269fba4666940832d.

---
+++ src/main/java/com/billkoch/example/jpa/repository/impl/AccountRepositoryJpaImpl.java
@@ -0,0 +1,22 @@
+package com.billkoch.example.jpa.repository.impl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+
+import com.billkoch.example.jpa.domain.Account;
+import com.billkoch.example.jpa.repository.AccountRepository;
+
+@Component
+public class AccountRepositoryJpaImpl implements AccountRepository {
+
+	@PersistenceContext
+	protected EntityManager entityManager;
+	
+	@Override
+	public String createNewAccount(Account account) {
+		entityManager.persist(account);
+		return account.getId();
+	}
+}
