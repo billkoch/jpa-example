159c1abcbdf185f511492831fe21a45452907813
Bill Koch <koch.149@osu.edu>
2011-07-27 23:02:56 -0400

    Moved the AccountRepository interface out of the impl package...not sure why I put it there in the first place...

--- src/main/java/com/billkoch/example/jpa/repository/impl/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.billkoch.example.jpa.repository.impl;
-
-import com.billkoch.example.jpa.domain.Account;
-
-public interface AccountRepository {
-
-	String createNewAccount(Account account);
-}
